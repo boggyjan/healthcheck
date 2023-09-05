38e75c6ad4f94cc161d35faa5bfd5e7d7e8c690f
Boggy Jang <boggy@idrip.coffee>
2023-09-05 10:45:16 +0800

    add admin page

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import fs from 'fs'
+import express from 'express'
+import bodyParser from 'body-parser'
 import axios from 'axios'
 import { CronJob } from 'cron'
+import md5 from 'md5'
+
+const port = 5454
+const app = express()
+app.use(bodyParser.json())
 
 const configPath = 'config.json'
-let slackToken = null
 let config = []
 
 function trigger () {
   const time = new Date().getMinutes() * 60 + new Date().getSeconds()
 
-  // 5s time % 60 === 5 (only in dev mode
-  // 30s time % 60 === 30
+  // Freqs
+  // 5s time % 5 === 0 (only in dev mode
+  // 10s time % 10 === 0
+  // 30s time % 30 === 0
   // 1m  time % 60 === 0
   // 5m  time % 300 === 0
   // 10m time % 600 === 0
@@ -41,38 +49,73 @@ function trigger () {
   if (time % 3600 === 0) {
     findSite('1h')
   }
-
-  // console.log('time change', time)
 }
 
 function findSite (freq) {
-  config.sites.filter(item => item.freq === freq).forEach(item => checkSite(item))
+  config.sites
+    .filter(item => item.freq === freq && item.status === 'on')
+    .forEach(item => checkSite(item))
 }
 
 async function checkSite (site) {
   try {
-    const res = await axios.get(site.url)
+    const res = await axios.get(
+      site.url,
+      {
+        timeout: site.timeout || 5000,
+      }
+    )
 
-    // if (res.status !== 200) {
-    //   throw new Error('非200')
-    // }
-    // console.log('恭喜，' + site.name + '還活著')
-  } catch (err) {
-
-    // if (err.response && err.response.status === 401) {
-    if (err.response) {
-      // console.log('恭喜，' + site.name + '還活著')
-      return
+    if (res.status !== site.expectStatusCode) {
+      const errorMsg = `非預期的status code，預期的是${site.expectStatusCode}，回應的是${res.status}`
+      sendMessageToSlack(site, errorMsg)
     }
-    // 這邊要透過slack回報
-    sendMessageToSlack(site)
-    // console.log(site.name + '倒站囉！！！')
+  } catch (err) {
+    if (err.response && err.response.status === site.expectStatusCode) {
+      return
+    } else if (err.response) {
+      const errorMsg = `非預期的status code，預期的是${site.expectStatusCode}，回應的是${err.response.status}`
+      sendMessageToSlack(site, errorMsg)
+    } else {
+      sendMessageToSlack(site, '網站沒有回應')
+    }
   }
 }
 
-// npm start時啟動一次
-// 隨著前台透過api更新資料後，再重啟一次
-function initHealthCheckerCronJob () {
+async function sendMessageToSlack (site, msg) {
+  try {
+    const url = 'https://slack.com/api/chat.postMessage'
+    const res = await axios.post(url, {
+      channel: '#' + (config.slackChannel || 'healthcheck'),
+      // 如何搞一個漂亮的排版：
+      // https://app.slack.com/block-kit-builder/
+      blocks: [
+        {
+          type: 'header',
+          text: {
+            type: 'plain_text',
+            text: `告訴你一個壞消息 ${site.name} 倒站囉！😱`
+          }
+        },
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: `🧭 網址：<${site.url}|${site.url}>    🕜 時間：${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}
+❕ 原因：${msg}`
+            }
+          ]
+        },
+        {
+          type: 'divider'
+        }
+      ]
+    }, { headers: { authorization: `Bearer ${config.slackToken}` } })
+  } catch {}
+}
+
+function getConfig () {
   if (!fs.existsSync(configPath)) {
     console.log('[Error] No config.json found')
     return
@@ -81,68 +124,107 @@ function initHealthCheckerCronJob () {
   const configStr = fs.readFileSync(configPath, 'utf-8')
 
   try {
+    // update config
     config = JSON.parse(configStr)
-    slackToken = config.slackToken
   } catch {
     console.log('[Error] config.json broken')
     return
   }
+}
 
-  // console.log(config)
+function checkAuth (user, pwd) {
+  const u = md5(user + pwd)
 
-  // 因為可以設定 30秒,1分鐘,5分鐘,10分鐘,30分鐘,1小時 打一次
-  // 因此最少每30秒要跑一次
+  for (let i = 0; i < config.users.length; i++) {
+    const item = config.users[i]
+
+    if (md5(item.user + item.pwd) === u) {
+      return u
+    }
+  }
+
+  return null
+}
+
+function checkToken (token) {
+  for (let i = 0; i < config.users.length; i++) {
+    const item = config.users[i]
+
+    if (md5(item.user + item.pwd) === token) {
+      return true
+    }
+  }
+
+  return false
+}
+
+function initHealthCheckerCronJob () {
+  getConfig()
   const job = new CronJob('0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', trigger)
   job.start()
   console.log('[Success] CronJob started')
 }
 
-// config設定內容
-// 網站名稱
-// 網址
-// 檢查頻率
-// 檢查項目
-// 逾時時間
-
+// init cron job
 initHealthCheckerCronJob()
 
-async function sendMessageToSlack (site) {
-  try {
-    const url = 'https://slack.com/api/chat.postMessage'
-    const res = await axios.post(url, {
-      channel: '#healthcheck',
-      // https://app.slack.com/block-kit-builder/
-      blocks: [
-        {
-          type: 'context',
-          elements: [
-            {
-              type: 'plain_text',
-              text: `😱 告訴你一個壞消息 ${site.name} 倒站囉！！！`
-              // emoji: true
-            }
-          ]
-        },
-        {
-          type: 'divider'
-        },
-        {
-          type: 'context',
-          elements: [
-            {
-              type: 'mrkdwn',
-              text: `🧭 網址：<${site.url}|${site.name}>  🕜 時間：${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`
-              // emoji: true
-            }
-          ]
-        },
-        {
-          type: 'divider'
-        }
-      ]
-    }, { headers: { authorization: `Bearer ${slackToken}` } })
-    // console.log(res)
-  } catch (err) {
-    // console.log(err)
+// Frontend console page
+app.get('/', (req, res) => res.send(fs.readFileSync('./pages/admin.html', 'utf-8')))
+
+// Frontend login
+app.post('/auth', (req, res) => {
+  if (req.body.user && req.body.pwd && config.users) {
+    const token = checkAuth(req.body.user, req.body.pwd)
+
+    if (token) {
+      res.send(`{"token":"${token}"}`)
+    } else {
+      res.send('{"message":"invalid user"}', 401)
+    }
+  } else {
+    res.send('{"message":"invalid user"}', 401)
   }
-}
+})
+
+// Frontend get config
+app.get('/config', (req, res) => {
+  if (!req.headers.authorization || !checkToken(req.headers.authorization)) {
+    res.send('{"message":"invalid user"}', 401)
+    return
+  }
+
+  if (!fs.existsSync(configPath)) {
+    res.send('{"users":[],slackToken":null,"slackChannel":"healthcheck","sites":[]}')
+    return
+  }
+
+  const configStr = fs.readFileSync(configPath, 'utf-8')
+  res.send(configStr)
+})
+
+// Frontend update config
+app.put('/config', (req, res) => {
+  if (req.headers.authorization && checkToken(req.headers.authorization)) {
+    const config = req.body
+    const configStr = JSON.stringify(config)
+
+    if (!config.users || !config.users.length) {
+      res.send('{"message":"no users data"}', 404)
+      return
+    } else if (!config.slackChannel || !config.slackToken) {
+      res.send('{"message":"no slack data"}', 404)
+      return
+    }
+
+    fs.writeFileSync(configPath, configStr, 'utf-8')
+    res.send(configStr)
+    getConfig()
+  } else {
+    res.send('{"message":"invalid user"}', 401)
+  }
+})
+
+// create server
+app.listen(port, () => {
+  console.log(`HealthCheck API and FrontEnd listening on port ${port}`)
+})
